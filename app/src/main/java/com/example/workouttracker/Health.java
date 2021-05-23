ecd1662d61bd873237861e95e3d7a44b120d5bb3
Yash Raj Pandey <55940078+devYRPauli@users.noreply.github.com>
2021-05-23 14:43:38 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/workouttracker/Health.java
@@ -0,0 +1,44 @@
+package com.example.workouttracker;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Health extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health);
+    }
+    public void onBackPressed(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void onWater(View view){
+        Intent intent = new Intent(this, Health_water.class);
+        startActivity(intent);
+        finish();
+    }
+    public void onSleep(View view){
+        Intent intent = new Intent(this,Health_sleep.class);
+        startActivity(intent);
+        finish();
+    }
+    public void onActive(View view){
+        Intent intent=new Intent(this,Health_active.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/workouttracker/Workout.java
@@ -0,0 +1,185 @@
+package com.example.workouttracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Workout extends AppCompatActivity{
+    SharedPreferences preferences ; //Save key-value data
+    SharedPreferences.Editor editor;    //Edit key-value data
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.workout);   //set the layout to workout xml file
+        /*SharedPreferences preferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        int myNumber;
+        myNumber = preferences.getInt("myNumber", 0);
+        myNumber++;
+        editor.putInt("myNumber", myNumber);
+        editor.commit();
+        TextView errorView = (TextView) findViewById(R.id.title);
+        errorView.setText(String.valueOf(myNumber));*/
+        preferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        editor = preferences.edit();
+        show();
+    }
+    public void update(){   //Update the display after some modifications were effectuated by the client
+        EditText c = (EditText) findViewById(R.id.editText);
+        String ex = c.getText().toString();
+        editor.putString("ex1",ex);
+        c = (EditText) findViewById(R.id.editText2);
+        ex = c.getText().toString();
+        editor.putString("ex2",ex);
+        c = (EditText) findViewById(R.id.editText3);
+        ex = c.getText().toString();
+        editor.putString("ex3",ex);
+        c = (EditText) findViewById(R.id.editText4);
+        ex = c.getText().toString();
+        editor.putString("ex4",ex);
+        editor.commit();
+    }
+    public void show(){ //Function to display the text and the numbers
+        int myNumber;
+        myNumber = preferences.getInt("c1", 0);
+        TextView c = (TextView) findViewById(R.id.count1);
+        c.setText(String.valueOf(myNumber));
+        myNumber = preferences.getInt("c2", 0);
+        c = (TextView) findViewById(R.id.count2);
+        c.setText(String.valueOf(myNumber));
+        myNumber = preferences.getInt("c3", 0);
+        c = (TextView) findViewById(R.id.count3);
+        c.setText(String.valueOf(myNumber));
+        myNumber = preferences.getInt("c4", 0);
+        c = (TextView) findViewById(R.id.count4);
+        c.setText(String.valueOf(myNumber));
+        String ex;
+        ex = preferences.getString("ex1","EMPTY");
+        c = (EditText) findViewById(R.id.editText);
+        c.setText(ex);
+        ex = preferences.getString("ex2","EMPTY");
+        c = (EditText) findViewById(R.id.editText2);
+        c.setText(ex);
+        ex = preferences.getString("ex3","EMPTY");
+        c = (EditText) findViewById(R.id.editText3);
+        c.setText(ex);
+        ex = preferences.getString("ex4","EMPTY");
+        c = (EditText) findViewById(R.id.editText4);
+        c.setText(ex);
+    }
+    // Add functions implement the adding operation for each button
+    public void Add1(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c1", 0);
+        myNumber++;
+        TextView c = (TextView) findViewById(R.id.count1);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c1", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Add2(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c2", 0);
+        myNumber++;
+        TextView c = (TextView) findViewById(R.id.count2);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c2", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Add3(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c3", 0);
+        myNumber++;
+        TextView c = (TextView) findViewById(R.id.count3);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c3", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Add4(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c4", 0);
+        myNumber++;
+        TextView c = (TextView) findViewById(R.id.count4);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c4", myNumber);
+        editor.commit();
+        update();
+    }
+    //Delete functions for each button
+    public void Del1(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c1", 0);
+        myNumber--;
+        TextView c = (TextView) findViewById(R.id.count1);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c1", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Del2(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c2", 0);
+        myNumber--;
+        TextView c = (TextView) findViewById(R.id.count2);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c2", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Del3(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c3", 0);
+        myNumber--;
+        TextView c = (TextView) findViewById(R.id.count3);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c3", myNumber);
+        editor.commit();
+        update();
+    }
+    public void Del4(View view){
+        int myNumber;
+        myNumber = preferences.getInt("c4", 0);
+        myNumber--;
+        TextView c = (TextView) findViewById(R.id.count4);
+        c.setText(String.valueOf(myNumber));
+        editor.putInt("c4", myNumber);
+        editor.commit();
+        update();
+    }
+    //Go back to main menu
+    public void onBackPressed(View view) {
+        EditText c = (EditText) findViewById(R.id.editText);
+        String ex = c.getText().toString();
+        editor.putString("ex1",ex);
+        c = (EditText) findViewById(R.id.editText2);
+        ex = c.getText().toString();
+        editor.putString("ex2",ex);
+        c = (EditText) findViewById(R.id.editText3);
+        ex = c.getText().toString();
+        editor.putString("ex3",ex);
+        c = (EditText) findViewById(R.id.editText4);
+        ex = c.getText().toString();
+        editor.putString("ex4",ex);
+        editor.commit();    //Save the data
+        Intent intent = new Intent(this, MainActivity.class);   //Manifest my intention to move to main menu
+        startActivity(intent);  //Start the main menu
+        finish();   //Terminate this activity
+    }
+}
